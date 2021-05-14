168ddd74c24933200f80cda49eb99fe0e37ae2dd
Sarah <sarahlchavez@gmail.com>
2021-05-13 20:45:16 -0700

    commit tree 13c0dc708a91c17291dc36deb75db2d3143f8158

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World"
+    end
+
+    get '/name' do
+        "My name is Sarah"
+    end
+
+    get '/hometown' do
+        "My hometown is Santa Rosa"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Under Pressure"
+    end
 end
